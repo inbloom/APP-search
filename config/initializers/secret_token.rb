68f0d24e1155660a018e3a80e9986f09c9c882dc
Jason Ellis <jason.ellis@yahoo.com>
2012-12-04 13:52:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LRMISearch::Application.config.secret_token = 'a4caffd6e5e5ca86556aeb760d049895ac0a4707f057ab95d094489aad51b684cc2c17a0b8248b7858f21d0e9cef7f904d8e4983c669153b955db43526614dee'
