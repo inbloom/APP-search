6f2d0d8b88ce94d6ce21ab1d8d00bce0f60a6ebc
Jason Ellis <jason.ellis@yahoo.com>
2012-12-04 15:13:19 -0600

    String prototype methods

---
+++ app/assets/javascripts/search/functions/string.js
@@ -0,0 +1,10 @@
+String.prototype.trunc = function(n,useWordBoundary) {
+    var toLong = this.length>n ,
+    s_ = toLong ? this.substr(0,n-1) : this;
+    s_ = useWordBoundary && toLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
+    return  toLong ? s_ + '&hellip;' : s_;
+};
+
+String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+};
