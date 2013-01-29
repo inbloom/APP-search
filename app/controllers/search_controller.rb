8992e14f3b74338823868083d684128b5607dadc
Jason Ellis <jason.ellis@yahoo.com>
2013-01-29 14:29:13 -0600

    That took many iterations but here we have a 80% solution for making searches of the LR and returning the results to the UI in json format.  Still need the or'ing and error catching done.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,19 +17,76 @@
 class SearchController < ApplicationController
 
   def full_search
-
+    # define the elasticsearch result "size" (limit)
+    limit = 10
+    # define the elasticsearch result "from" (offset)
+    offset = 0
+    # Default output
+    searchResults = ''
+    # Search Engine (should put this in the config)
     url = 'http://esearch.agilix.com:9200/lr/lr/_search'
 
-    puts "params"
-    puts params
+    # If we have filters, we need to parse them
+    if params['filters'].present?
 
+      filters = []
+      # For each of the filters format them and stuff them into an array
+      params['filters'].each do |key, filter|
+        if filter.keys.count > 1
+          # This is more complex because this filter type needs the keys or'd together
+          # @TODO Handle the or'ing of multiple filters together
+        else
+          # This should be simple, there is only one of this filter key
+          filters << { 'query' => { 'match' => { key => filter.keys.first.to_s } } }
+        end
+      end
 
-    payload = '{"query":{"match":{"targetName":"CCSS.Math.Content.6.EE.A.3"}}}'
+      # If the query is present we need to match it
+      if params['query'].present?
+        query = { 'match' => { '_all' => params['query'] } }
+        filter = { 'limit' => { 'value' => 100 }, 'and' => filters }
+      # If no query is present then we can wildcard against anything
+      else
+        query = { 'wildcard' => { '_all' => '?' } }
+        filter = { 'limit' => { 'value' => 100 }, 'and' => filters }
+      end
 
-    searchResults = RestClient::Request.new( :method => :get, :url => url, :payload => payload ).execute
+    # if not filter is present then just match against query
+    else
+      query = { 'match' => { '_all' => params['query'] } }
+      filter = { 'limit' => { 'value' => 100 } }
+    end
 
-    respond_to do |format|
-      format.json { render json: searchResults }
+    # Build the payload from the various parts
+    payload = {
+        'size' => limit,
+        'from' => offset,
+        'query' => {
+            'filtered' => {
+                'query' => query,
+                'filter' => filter
+            }
+        }
+    }
+
+    # puts "PAYLOAD"; puts payload.to_json
+
+    # Okay after all that mess, lets make the request
+    request = RestClient::Request.new( :method => :get, :url => url, :payload => payload.to_json )
+
+    # Since this can error lets catch it
+    begin
+      searchResults = request.execute
+      respond_to do |format|
+        format.json { render json: searchResults }
+      end
+      #puts "RESPONSE"; puts searchResults
+    rescue => e
+      # @TODO Need to return the correct error type and then an error message to be shown to user.
+      respond_to do |format|
+        format.json { render json: searchResults }
+      end
+      #puts "ERROR!"; puts e.response
     end
 
   end
